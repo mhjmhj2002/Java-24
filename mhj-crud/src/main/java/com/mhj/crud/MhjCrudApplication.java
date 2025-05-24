cec5e67701b4bd00744133d7e08183448e3eaed2
mhjmhj2002 <mhjmhj2002@gmail.com>
2025-05-24 19:13:51 -0300

    first commit

---
+++ mhj-crud/src/main/java/com/mhj/crud/MhjCrudApplication.java
@@ -0,0 +1,13 @@
+package com.mhj.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhjCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhjCrudApplication.class, args);
+	}
+
+}
